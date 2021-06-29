54650e6e41b6e3eabfd019748430a8b3894e5707
Fastion-1024 <dsprice1989@outlook.com>
2021-06-29 14:56:21 +0100

    Added all child component to App Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import QuoteBox from './Components/QuoteBox';
+import Filters from './Components/Filters';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import { useGlobalContext } from './context';
+
 function App() {
-    return <div></div>;
+    const { loading } = useGlobalContext();
+
+    if (loading) {
+        return (
+            <main>
+                <h1>Loading...</h1>
+            </main>
+        );
+    }
+    return (
+        <main>
+            <Container>
+                <Col>
+                    <QuoteBox />
+                    <Filters />
+                </Col>
+            </Container>
+        </main>
+    );
 }
 
 export default App;

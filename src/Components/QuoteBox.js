932a185b41a0112ca1c07d38d0cb59f6333a4f5c
Fastion-1024 <dsprice1989@outlook.com>
2021-07-01 17:36:37 +0100

    Updated Layout and Style of QuoteBox - Added font awesome icons for twitter and filters. - Added layout to buttons. - Updated classNames for new stylesheet

--- src/Components/QuoteBox.js
+++ src/Components/QuoteBox.js
@@ -1,30 +1,43 @@
 import { useGlobalContext } from '../context';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import { FaTwitter, FaFilter } from 'react-icons/fa';
 
 const QuoteBox = () => {
     const { quote, fetchQuote } = useGlobalContext();
     return (
         <section id='quote-box'>
             <figure>
-                <blockquote className='blockquote'>
+                <blockquote className='blockquote text-center'>
                     <span id='text'>{quote.content}</span>
                 </blockquote>
-                <figcaption id='author' className='blockquote-footer'>
+                <figcaption id='author' className='blockquote-footer text-end'>
                     {quote.author}
                 </figcaption>
             </figure>
+
+            <Container className='d-flex justify-content-start'>
+                <Button className='secondary-btn'>
+                    <FaFilter />
+                </Button>
+
+                <Button
+                    id='tweet-quote'
+                    className='secondary-btn'
+                    href={
+                        'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
+                        encodeURIComponent(
+                            '"' + quote.quote + '" ' + quote.author
+                        )
+                    }
+                    target='_blank'
+                >
+                    <FaTwitter />
+                </Button>
+            </Container>
+
             <Button id='new-quote' onClick={fetchQuote}>
                 New Quote
-            </Button>
-            <Button
-                id='tweet-quote'
-                href={
-                    'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
-                    encodeURIComponent('"' + quote.quote + '" ' + quote.author)
-                }
-                target='_blank'
-            >
-                Tweet
             </Button>
         </section>
     );
